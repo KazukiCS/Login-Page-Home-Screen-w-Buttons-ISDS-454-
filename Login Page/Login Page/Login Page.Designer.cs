dd0ecf0518f01df670c3117624fb2e3aad061d7d
KazukiCS <71460534+KazukiCS@users.noreply.github.com>
2024-04-30 19:51:05 -0700

    Add files via upload

---
+++ Login Page/Login Page/Login Page.Designer.cs
@@ -0,0 +1,92 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+
+namespace Login_Page
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.RichTextBox();
+            this.Password = new System.Windows.Forms.RichTextBox();
+            this.Log = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Username.Location = new System.Drawing.Point(641, 204);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(191, 26);
+            this.Username.TabIndex = 0;
+            this.Username.Text = "";
+            // 
+            // Password
+            // 
+            this.Password.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Password.Location = new System.Drawing.Point(641, 263);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(191, 26);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "";
+            // 
+            // Log
+            // 
+            this.Log.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.Log.Location = new System.Drawing.Point(689, 311);
+            this.Log.Name = "Log";
+            this.Log.Size = new System.Drawing.Size(101, 31);
+            this.Log.TabIndex = 2;
+            this.Log.Text = "Login";
+            this.Log.UseVisualStyleBackColor = true;
+            this.Log.Click += new System.EventHandler(this.Log_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Login_Page.Properties.Resources.pokemon_ball_colors_simple_background_1440x900;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(844, 499);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Username;
+        private System.Windows.Forms.RichTextBox Password;
+        private System.Windows.Forms.Button Log;
+    }
+}
+
+
+
---
+++ Login Page/Login Page/Login Page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Page
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Log_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Home f2 = new Home();
+            f2.Show();
+        }
+    }
+}
